ac23542590f4a8c96fc1fc80986fcec8b5c2f842
yuting-cheng <yuting841008@gmail.com>
2023-06-10 16:25:46 +0800

    ＯＯ

--- lesson12_1.py
+++ lesson12_1.py
@@ -1,14 +1,25 @@
+
 import streamlit as st
 import pandas as pd
+import yfinance as yf
 
 codeFrame = pd.read_csv('codeSearch.csv',usecols=['code','name'])
 codeSeries = codeFrame['code'].astype(str) + codeFrame['name']
 
 with st.sidebar:
-    selected_code = st.multiselect("請選擇股票(可複選):",codeSeries,max_selections=4)
+    selected_codes = st.multiselect("請選擇股票:",codeSeries,max_selections=4)
+    
+@st.cache_data
+def fetch_stock_dataFrame(id):
+    stock_dataFrame = yf.download(id,start='2022-01-01')
+    return stock_dataFrame
 
+chartDataFrame = None
 
-
-for code in selected_code:
-    code = code[:4]+'.TW' #此為取code前四位字＋TW 顯示
-    st.write(code)
+for code in selected_codes:
+    code1 = code[:4]+'.TW'
+    code_stock_dataFrame = fetch_stock_dataFrame(code1)
+    code_stock_dataFrame_sorted = code_stock_dataFrame.sort_index(ascending=False)
+    st.line_chart(code_stock_dataFrame_sorted,y='Adj Close')
+    st.dataframe(code_stock_dataFrame_sorted,width=1024)    
+    st.divider()
